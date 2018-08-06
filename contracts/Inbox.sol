0566cf176754aa879a0929b0e388fdb502060ad8
Bryan Leung <blkclkc@gmail.com>
2018-08-06 12:50:38 -0400

    deploying contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.17;
 
 contract Inbox {
-    string public message;
+    string public message; //will create a function message() for returning the variable
     
     //constructor function has the same name as the class
     function Inbox(string initialMessage) public {
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,24 +1,40 @@
 const assert = require('assert');
-const ganache = require('ganache-cli'); //ethereum test network
+const ganache = require('ganache-cli'); //ethereum local test network
 const Web3 = require('web3'); //constructor for interacting with deployed contracts
-const web3 = new Web3(ganache.provider()); //instance of Web3 with the network's provider
+
+//instance of Web3 with the network's provider
+const provider = ganache.provider();
+const web3 = new Web3(provider); 
+
 const { interface, bytecode } = require('../compile'); //properties of the contract object
 
 let accounts;
 let inbox;
 
 beforeEach(async () => {
-	//get a list of all accounts
-	accounts = await web3.eth.getAccounts();
+	accounts = await web3.eth.getAccounts(); //get a list of all accounts
 
 	//use one of those accounts to deploy the contact
 	inbox = await new web3.eth.Contract(JSON.parse(interface))
-		.deploy({ data: bytecode, arguments: ['Hi there!'] })
+		.deploy({ data: bytecode, arguments: ['Hi there!'] }) //arguments for the constructor function
 		.send({ from: accounts[0], gas: '1000000' })
+
+	inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
 	it('deploys a contact', () => {
-		console.log(inbox);
+		assert.ok(inbox.options.address); //verify that there is an address after deployment
+	});
+
+	it('has a default message', async () => {
+		const message = await inbox.methods.message().call();
+	    assert.equal(message, 'Hi there!'); //verify that the initial argument is in the contract
+	});
+
+	it('can change the message', async () => {
+		await inbox.methods.setMessage('bye').send({ from: accounts[0] }); //receives transaction hash (like a receipt)
+		const message = await inbox.methods.message().call(); //have to call method again to verify
+		assert.equal(message, 'bye');
 	});
 });
